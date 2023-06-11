c1ad10eaa7fd44521a0caf5c97715367a2dbd0d9
HugeLol556 <89110122+joshuadevp@users.noreply.github.com>
2023-06-10 20:01:15 -0700

    Added survival objective
    Player must stay within a radius of objective for X time while darkness spreading and enemy spawning is increased

---
+++ Assets/Scripts/Objectives/SurviveObjective.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class SurviveObjective : ScriptableObject, IObjective
+{
+    [SerializeField]
+    DarknessSettings globalSettings;
+    [SerializeField]
+    float removeDarknessRadius;
+    [SerializeField]
+    float playerTetherDistance;
+    [SerializeField]
+    float timeToSurvive;
+    [SerializeField]
+    float newGlobalSpawnChance;
+    [SerializeField]
+    float newGlobalSpreadChance;
+    private GameObject player;
+    private bool completed = false;
+    private Vector2 loc;
+    private float timer;
+    private float oldSpreadChance;
+    private float oldSpawnChance;
+
+    public void ManualUpdate()
+    {
+        if (!completed && Vector3.Magnitude(player.transform.position - (Vector3)loc) < playerTetherDistance)
+        {
+            globalSettings.DarknessSpreadChance = newGlobalSpreadChance;
+            globalSettings.EnemySpawnChance = newGlobalSpawnChance;
+            timer += Time.deltaTime;
+        }
+        if(timer > timeToSurvive)
+        {
+            globalSettings.DarknessSpreadChance = oldSpreadChance;
+            globalSettings.EnemySpawnChance = oldSpawnChance;
+            completed = true;
+        }
+    }
+
+    public void Setup(Vector2 loc)
+    {
+        this.loc = loc;
+        FogOfDarknessManager manager = FindObjectOfType<FogOfDarknessManager>();
+        manager.RemoveDarknessPointsCircle(loc, removeDarknessRadius);
+        // TODO: CHANGE TO TYPE OF ACTUAL PLAYER SCRIPT
+        player = GameObject.FindFirstObjectByType<MoveCube>().gameObject;
+        oldSpawnChance = globalSettings.EnemySpawnChance;
+        oldSpreadChance = globalSettings.DarknessSpreadChance;
+    }
+
+    public bool Completed()
+    {
+        return completed;
+    }
+
+    public Vector2 GetLocation()
+    {
+        return loc;
+    }
+}
