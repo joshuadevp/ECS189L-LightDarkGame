8bb82151c8915b462b6e9caefca497027a8202d2
HugeLol556 <89110122+joshuadevp@users.noreply.github.com>
2023-06-11 17:50:20 -0700

    Misc Main Menu Items

---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [SerializeField]
+    DarknessSettings defaultDarknessSettings;
+    [SerializeField]
+    DarknessSettings globalDarknessSettings;
+    [SerializeField]
+    FogOfDarknessManager fogManager;
+    [SerializeField]
+    GameObject camera;
+    
+    void OnEnable()
+    {
+        globalDarknessSettings.EnemySpawnChance = 0f;
+        globalDarknessSettings.DarknessSpreadChance = 0.3f;
+        this.transform.position = camera.transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        if(Random.Range(0f,1f) < 0.01f)
+        {
+            fogManager.RemoveDarknessPointsCircle(camera.transform.position+new Vector3(Random.Range(-50f,50f),Random.Range(-50f,50f),0),25);
+        }
+    }
+}
